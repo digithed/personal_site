4a462a3932240dab92a67b5a55c3ff5e144851e1
digithed <nickrosenau1@gmail.com>
2022-07-24 14:37:01 -0400

    visual improvements.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import { FiMail } from "react-icons/fi";
 
 function App() {
   return (
-    <div className='h-screen bg-gradient-to-r from-rose-200 to-orange-300'>
+    <div className='bg-gradient-to-r from-rose-200 to-orange-300'>
       <SideBar />
       <div className='pl-40 container mx-auto'>
 
         <div className='pt-10 place-content-center flex'>
-          <div style={{ 'width': '700px' }} className='text-title rounded-xl shadow-lg shadow-white p-5'>
-            <div className='flex flex-row float-right space-x-4'>
+        <div className='mr-40 mb-20 p-10'>
+        <div style={{'top': '10px' }} className='absolute z-40'>
+                <img alt="me" style={{ 'width': '250px', 'height': '250px'}} className='picture-fade rounded-full shadow-xl' src='me.png'></img>
+              </div>
+          <div style={{ 'width': '400px', 'height': '350px', 'top': '100px', 'bottom': '200px', 'left': '150px' }} className='relative text-title rounded-xl shadow-lg shadow-orange-200 p-5'>
+          
+              <div>
+                <h1 className='pl-5 text-title font-serif pt-10 text-4xl font-bold'>Nick Rosenau</h1>
+                <h3 className='pl-5 text-subtitle font-mono pt-10 text-lg font-bold'>Full-stack software engineer</h3>
+                <h3 className='pl-5 pb-5 text-subtitle font-serif text-xl font-bold'>Life sciences aficionado</h3>
+                <h4 className='pl-5 pb-5 text-subtitle font-serif text-lg font-bold'>Engineer @ <a className='text-orange-500' href='https://latticeautomation.com/'>Lattice Automation</a></h4>
+                {/* <h3 className='p-5 text-extra italic font-serif pt-10 text-2xl font-bold shadow-sm'>Funky AND Fresh</h3> */}
+              </div>
+              <div className='flex flex-row float-right space-x-4'>
               <a href='https://github.com/digithed'><img className={'w-7 h-7 shadow-xl hover:shadow-white'} alt='github logo' src='github-logo.png' /></a>
               <a href='https://www.linkedin.com/in/nick-rosenau-2b0614b3/'><img className={'w-7 h-7 shadow-xl hover:shadow-white'} alt='linkedin logo' src='linkedin-logo.png' /></a>
               <a href='mailto: nicholas.c.rosenau@gmail.com'>
@@ -23,20 +35,10 @@ function App() {
                
                 </a>
             </div>
-            <div className='flex flex-row '>
-              <div>
-                <h1 className='pl-5 text-title font-serif pt-10 text-4xl font-bold'>Nick Rosenau</h1>
-                <h3 className='pl-5 text-subtitle font-mono pt-10 text-lg font-bold'>Full-stack software engineer</h3>
-                <h3 className='pl-5 pb-5 text-subtitle font-serif text-xl font-bold'>Life sciences aficionado</h3>
-                {/* <h3 className='p-5 text-extra italic font-serif pt-10 text-2xl font-bold shadow-sm'>Funky AND Fresh</h3> */}
               </div>
-              <div className='pl-10 mt-5'>
-                <img alt="me" style={{ 'width': '200px', 'height': '200px' }} className='picture-fade rounded-full shadow-xl' src='me.png'></img>
-              </div>
-            </div>
           </div>
         </div>
-        <div className='projects mt-10 pb-10'>
+        <div style={{'paddingTop': '75px'}} className='projects pb-10'>
           <div className='pb-1 pl-5 font-serif text-2xl font-bold'>Projects</div>
           <hr className='pb-5'></hr>
           <div className='place-content-center flex flex-row space-x-10'>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,7 +6,7 @@ function SideBar() {
         <div className='navBar flex flex-col bg-slate-700 shadow-xl'>
             <h1 className="text-center pt-5 font-serif text-white">nickrosenau.com</h1>
             <Link to="/">
-            <div className="mt-10 pt-2 pb-2 flex flex-row hover:bg-orange-200 ">
+            <div className="mt-10 pt-2 pb-2 flex flex-row hover:bg-orange-300 ">
                 <div className="pl-3 pt-0.5 text-white">
                     <FiHome size={'1.25em'} />
                 </div>
@@ -14,7 +14,7 @@ function SideBar() {
             </div>
             </Link>
             <Link to="/about">
-            <div className="mt-5 pt-2 pb-2 flex flex-row hover:text-black hover:bg-orange-200">
+            <div className="mt-5 pt-2 pb-2 flex flex-row hover:text-black hover:bg-orange-300">
                 <div className="pl-3 pt-0.5 text-white">
                     <FiUser size={'1.25em'} />
                 </div>
