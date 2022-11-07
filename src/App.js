1f3033e493c9fdd172df08c7f06e1245ec6f1f14
digithed <nickrosenau1@gmail.com>
2022-11-07 04:19:46 -0500

    fun changes.

--- src/App.js
+++ src/App.js
@@ -14,15 +14,8 @@ function App() {
 
           <div className='mb-20 p-10 relative z-101'>
 
-            <div style={{ 'width': '640px', 'height': '490px' }} className='relative card-background text-title rounded-xl shadow-slate-200 shadow-lg p-5'>
             <div style={{ 'width': '600px', 'height': '450px' }} className='relative card-background-inner text-title rounded-xl shadow-lg p-5'>
               <div>
-                <div className='absolute bottom-0 left-0 mb-10'>
-                  {/* <FoxOne /> */}
-                </div>
-                <div className='absolute bottom-10 right-0 z-0'>
-                  {/* <Robot eye1={'eye1'} eye2={'eye2'} sensor={"sensor"} /> */}
-                </div>
                 <img alt="me" style={{ 'width': '180px', 'height': '150px' }} className='relative ml-auto mr-auto picture-fade rounded-xl shadow-xl' src='me.png'></img>
               </div>
               <div className='text-center'>
@@ -30,22 +23,21 @@ function App() {
                 <h3 className='pl-5 text-subtitle font-mono pt-5 text-lg font-bold'>Software engineer, inventor, entrepreneur</h3>
 
                 <h3 className='pl-5 text-subtitle font-mono pt-5 text-sm font-bold'>Programming Languages, Machine Learning, Life Sciences</h3>
-                <p className='text-xs mt-3 text-slate-400 subtext'>SVG animations created by Nick Rosenau</p>
+              <br></br>
               </div>
               <div className='flex flex-row place-content-center pt-3 space-x-4'>
-                <a href='https://github.com/digithed'><img className={'w-7 h-7 shadow-xl hover:shadow-slate-300'} alt='github logo' src='github-logo.png' /></a>
-                <a href='https://www.linkedin.com/in/nick-rosenau-2b0614b3/'><img className={'w-7 h-7 shadow-xl hover:shadow-slate-300'} alt='linkedin logo' src='linkedin-logo.png' /></a>
+                <a href='https://github.com/digithed'><img className={'w-7 h-7 shadow-xl hover:shadow-teal-300'} alt='github logo' src='github-logo.png' /></a>
+                <a href='https://www.linkedin.com/in/nick-rosenau-2b0614b3/'><img className={'w-7 h-7 shadow-xl hover:shadow-teal-300'} alt='linkedin logo' src='linkedin-logo.png' /></a>
                 <a href='mailto: nicholas.c.rosenau@gmail.com'>
                   <span class="flex h-5 w-5 float-right">
-                    <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-slate-300 opacity-75"></span>
+                    <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-teal-300 opacity-75"></span>
                     <span class="relative inline-flex rounded-full h-3 w-3 bg-green-400"></span>
                   </span>
-                  <FiMail className='shadow-xl hover:shadow-slate-300' size={'1.85em'} />
+                  <FiMail className='shadow-xl hover:shadow-teal-300' size={'1.85em'} />
 
                 </a>
               </div>
             </div>
-          </div>
           </div>
         </div>
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,18 +9,25 @@ function Projects() {
             <NavBar />
 
             <div className='projects p-20'>
-                <div className='pb-1 pl-5 text-2xl font-bold'>Projects</div>
+                <div className="flex flex-row space-x-10">
+                <div className='pb-1 text-4xl font-bold'>Projects</div>
+                <img width='50px' height='50px' src='worker.png' alt='worker'/>
+                </div>
                 <hr className='pb-5'></hr>
+                <div className="mb-5 font-mono text-lg">
+                    <h2>"If you ever feel overwhelmed, take on another project."</h2>
+                    <i>- Roy Halston Frowick</i>
+                    </div>
                 <div className='place-content-center flex flex-row space-x-10 z-100'>
                     <ProjectCard project_name={'Mona'} link={""} project_description={'Working on a programming language that creates experimental designs'} icon={'wave.png'} />
                     <ProjectCard project_name={'Crispier and Cas-ier'} link={""} project_description={'Creating a CRISPR web application based on a newly designed Cas protein'} icon={'dna.png'} />
                     <ProjectCard project_name={'Component Library'} link={""} project_description={'Creating a component library for React'} icon={'dna.png'} />
                 </div>
             </div>
-            <div className='absolute top-10 z-0'>
+            <div className='absolute top-20 z-0'>
                 <FoxTwo />
             </div>
-            <div className='absolute mt-10 top-40 right-0 z-0'>
+            <div className='absolute mt-20 top-40 right-0 z-0'>
                 <Robot eye1={'eye1angry'} eye2={'eye2angry'} sensor={"angrysensor"} />
             </div>
         </div>
