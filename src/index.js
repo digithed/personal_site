9e5c44bd3e1ce0eb0010e66db9eb33f0b5594a4b
digithed <nickrosenau1@gmail.com>
2023-01-03 01:00:36 -0500

    remove routes, update icon.

--- src/index.js
+++ src/index.js
@@ -9,16 +9,12 @@ import {
   Route,
 } from "react-router-dom";
 
-import About from './components/About';
-import Projects from './components/Projects';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
     </Routes>
   </BrowserRouter>
 );
