a383d7d49b7db254aef08554889db7a77f2f180f
Nick Rosenau <nickrosenau1@gmail.com>
2023-11-02 17:31:18 -0400

    fix zoom.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import './IconMove.css';
-
+import { Head } from 'next/document'
 function MyApp({ Component, pageProps }) {
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </Head>
   return <Component {...pageProps} />
 }
 
